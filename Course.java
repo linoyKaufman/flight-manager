33036b01f7aad44935ef9704d235908094752530
linoyKaufman <kaufmanlinoy@gmail.com>
2024-05-04 23:42:51 +0300

    push

--- Course.java
+++ Course.java
@@ -1,17 +1,35 @@
+import java.util.List;
+import flightchanges.Observer;
+
 public class Course {
     
     private String courseName;
     private String courseType;
     private int numofplaces;
+    private List<Observer> registered;
+    private List<Student> students; 
+    private String courseID; 
 
-    public Course(String courseName, String courseType, int numofplaces) {
+    public Course(String courseName, String courseType, int numofplaces,List<Observer> registered,List<Student> students, String courseID) {
         this.courseName = courseName;
         this.courseType = courseType;
         this.numofplaces = numofplaces;
+        this.registered = registered;
+        this.students = students;
+        this.courseID = courseID;
+        
     }
 
     public String getCourseName() {
         return courseName;
+    }
+
+    public List<Student> getStudentList(){
+        return students;
+    }
+
+    public String getCourseID(){
+        return courseID;
     }
 
     public String getCourseType() {
@@ -25,5 +43,9 @@ public class Course {
     public void displayInfo() {
         System.out.println("Course Name: " + courseName);
         System.out.println("Course Type: " + courseType);
+    }
+  
+    public void register(Observer student) {
+       registered.add(student);
     }
 }
