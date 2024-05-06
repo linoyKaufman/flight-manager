e03a2a94030dfeede562c2d15b328f337e28eb0c
linoyKaufman <kaufmanlinoy@gmail.com>
2024-05-06 22:34:14 +0300

    push

--- EX2/Main.java
+++ EX2/Main.java
@@ -1,25 +1,43 @@
-package EX2;
+    package EX2;
 
-import EX2.course.Course;
-import EX2.course.CourseManagement;
-import EX2.staff.Lecturer;
-import EX2.staff.Student;
+    import java.util.Scanner;
+    import EX2.course.Course;
+    import EX2.course.CourseManagement;
+    import EX2.staff.Lecturer;
+    import EX2.staff.Student;
 
-public class Main {
-    public static void main(String[] args){
-    SchoolStuffSingeltone schoolStuff = SchoolStuffSingeltone.getInstance();
-    CourseManagement courseManagement = new CourseManagement();
-    Student linoy = new Student("linoy", 25, 123, 567, "computer");
-    Lecturer amit = new Lecturer("amit", 45, 785, "computer");
-    schoolStuff.addMemberToStuff(amit);
-    schoolStuff.addMemberToStuff(linoy);
-    Course c1 = amit.createCourse(courseManagement, 5869, "computer", "seminar", 1);
-    Course c2 =amit.createCourse(courseManagement, 888820, "english", "must", 2);
-    Student david = new Student("david", 25, 8587, 88878, "computer");
-    linoy.registertoCourse(c1);
-    david.registertoCourse(c1);
-    linoy.unregistertoCourse(c1);
+    public class Main {
+        public static void main(String[] args){
+        
+        // Create a singleton instance of SchoolStuffSingleton
+        SchoolStuffSingeltone schoolStuff = SchoolStuffSingeltone.getInstance();
+        
+        // Create an instance of CourseManagement
+        CourseManagement courseManagement = new CourseManagement();
+        
+        // Create instances of Student and Lecturer
+        Student linoy = new Student("linoy", 25, 123, 567, "computer");
+        Lecturer amit = new Lecturer("amit", 45, 785, "computer");
+        Student david = new Student("david", 25, 8587, 88878, "computer");
+        
+        // Add members to the staff using the singleton instance
+        schoolStuff.addMemberToStuff(amit);  
+        schoolStuff.addMemberToStuff(linoy);
+        schoolStuff.addMemberToStuff(david);
+        
+        // Create courses using the Lecturer object       
+        Course c1 = amit.createCourse(courseManagement, 5869, "computer", "seminar", 1);
+        Course c2 =amit.createCourse(courseManagement, 888820, "english", "must", 2);
+        
+        // Register students to courses
+        linoy.registertoCourse(c1);
+        david.registertoCourse(c1);
+        // Unregister a student from a course
+        linoy.unregistertoCourse(c1);
 
-}
+
     
-}
+
+    }
+    
+    }
